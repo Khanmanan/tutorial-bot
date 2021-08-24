d3c284476fcfd507ab3dd28d4149d34e36608af8
Cwkhan <65243374+Khanmanan@users.noreply.github.com>
2021-08-24 18:51:42 +0530

    Updated to v13

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 require('dotenv').config();
 const discord = require("discord.js");
 
-const client = new discord.Client({
-    disableEveryone: true
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
 client.commands = new discord.Collection();
 client.aliases = new discord.Collection();
 
